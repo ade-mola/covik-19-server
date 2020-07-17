984bd2f29be62aa88525e403439bba993175980e
noguntuberu <nateoguns.work@gmail.com>
2020-07-17 01:19:04 +0100

    patient-cluster tracking integrated.

--- src/services/patient-tracking/patient-clusters.ts
+++ src/services/patient-tracking/patient-clusters.ts
@@ -1,13 +1,45 @@
 /**
  * @author EDC: Oguntuberu Nathan O. <nateoguns.work@gmail.com>
  */
+import { ICluster } from "../../interfaces/Cluster";
+const ClusterController = require('../../controllers/Cluster');
 
 class PatientCluster {
-    getListOfPossibleCasesForGivenUser(userId: number, timeJoined: string): Array<number> {
-        return [];
+    //
+    visitedCases: Map<string, boolean> = new Map;
+    possibleCases: Map<string, boolean> = new Map;
+    patientId: string = '';
+
+    //
+    constructor(patientId: string) {
+        this.patientId = patientId;
     }
 
-    addUserCluster(userId: number, clusters: Array<any>): void {
+    async getListOfPossibleCasesForGivenUser(userId: string, baseTime: string, isStart: boolean = false) {
+        if (this.visitedCases.has(userId)) return;
+        if ((userId == this.patientId) && !isStart) return;
 
+        this.visitedCases.set(userId, true); // mark user as visited.
+        const clusters: Array<ICluster> = await ClusterController.readMany({
+            [`users.${userId}`]: { timeJoined: { $gte: baseTime } },
+        });
+
+        const users = clusters.reduce((all_users: any, cluster) => {
+            return [ ...all_users, ...cluster.users ];
+        }, []);
+
+        //
+        users.forEach(async (user: any) => {
+            if (user.timeLeft >= baseTime) {
+                this.possibleCases.set(user, true);
+                await this.getListOfPossibleCasesForGivenUser(user.userId, user.timeLeft);
+            }
+        });
+
+        //
+        return;
     }
 }
+
+export default PatientCluster;
+
