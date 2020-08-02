e1c030282beb0ec99957a4f236be198fa781bf55
Oluwatobi <oluwatobiadeoye18@gmail.com>
2020-08-02 23:34:59 +0100

    add notification token to user model

---
+++ src/services/user/User.ts
@@ -0,0 +1,40 @@
+/**
+ * @author EDC: Oguntuberu Nathan O. <nateoguns.work@gmail.com>
+*/
+
+import UserController from '../../controllers/User';
+import ResponseUtility from '../../utilities/Response';
+import Logger from '../../utilities/Logger';
+import { IUser } from '../../interfaces/User';
+import { IHttpResponse } from '../../interfaces/HTTPRepsonse';
+
+// Write Heavy Logic in services
+class UserService {
+    private userControl: any;
+    
+    constructor() { 
+        this.userControl = UserController
+    }
+
+    async sampleRequestHandler(request: Request) {
+        try {
+            return ResponseUtility.processFailedResponse(404, 'Resource Not Found');
+        } catch (e) {
+            console.log(`[UserService] inHouseMethod Error: ${e.message}`);
+        }
+    }
+
+    async updateUserNotificationToken(userId: string, notificationToken: string): Promise<IHttpResponse> {
+        if (!userId || !notificationToken) return ResponseUtility.processFailedResponse(400, 'Invalid request data');
+
+        const user = await this.userControl.readOne({ user_id: userId })
+        if (!user.success) {
+            Logger.info(`User with id: ${userId} does not exit`)
+            return ResponseUtility.processFailedResponse(400, 'Invalid user');
+        }
+
+        return await this.userControl.update({notificationToken}, user.payload as IUser);
+    }
+}
+
+export default new UserService; // or new UserService
